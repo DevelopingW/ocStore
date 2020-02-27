8343cbbc089e8cb0e704fb23174a58671f872945
Anton Kalochelitis <fire.anton@gmail.com>
2020-02-27 02:42:13 +0200

    Добавляем Превью для товара в списке производителя;

--- upload/catalog/controller/product/manufacturer.php
+++ upload/catalog/controller/product/manufacturer.php
@@ -216,16 +216,17 @@ class ControllerProductManufacturer extends Controller {
 				}
 
 				$data['products'][] = array(
-					'product_id'  => $result['product_id'],
-					'thumb'       => $image,
-					'name'        => $result['name'],
-					'description' => utf8_substr(trim(strip_tags(html_entity_decode($result['description'], ENT_QUOTES, 'UTF-8'))), 0, $this->config->get('theme_' . $this->config->get('config_theme') . '_product_description_length')) . '..',
-					'price'       => $price,
-					'special'     => $special,
-					'tax'         => $tax,
-					'minimum'     => $result['minimum'] > 0 ? $result['minimum'] : 1,
-					'rating'      => $result['rating'],
-					'href'        => $this->url->link('product/product', 'manufacturer_id=' . $result['manufacturer_id'] . '&product_id=' . $result['product_id'] . $url)
+					'product_id'            => $result['product_id'],
+					'thumb'                 => $image,
+					'name'                  => $result['name'],
+					'description_preview'   => utf8_substr(trim(html_entity_decode($result['description_preview'], ENT_QUOTES, 'UTF-8')), 0, $this->config->get('theme_' . $this->config->get('config_theme') . '_product_description_length')),
+					'description'           => utf8_substr(trim(strip_tags(html_entity_decode($result['description'], ENT_QUOTES, 'UTF-8'))), 0, $this->config->get('theme_' . $this->config->get('config_theme') . '_product_description_length')) . '..',
+					'price'                 => $price,
+					'special'               => $special,
+					'tax'                   => $tax,
+					'minimum'               => $result['minimum'] > 0 ? $result['minimum'] : 1,
+					'rating'                => $result['rating'],
+					'href'                  => $this->url->link('product/product', 'manufacturer_id=' . $result['manufacturer_id'] . '&product_id=' . $result['product_id'] . $url)
 				);
 			}
 
