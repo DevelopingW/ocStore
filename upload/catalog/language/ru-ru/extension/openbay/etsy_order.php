b3f2ae9566e8894f8aea61a97fb5cb9128282923
Anton Kalochelitis <fire.anton@gmail.com>
2020-02-27 03:10:17 +0200

    Устанавливаем верный перевод

--- upload/catalog/language/ru-ru/extension/openbay/etsy_order.php
+++ upload/catalog/language/ru-ru/extension/openbay/etsy_order.php
@@ -5,6 +5,6 @@
 // Text
 $_['text_total_shipping']		= 'Доставка';
 $_['text_total_discount']		= 'Скидка';
-$_['text_total_tax']			= 'Без НДС';
+$_['text_total_tax']			= 'Налог';
 $_['text_total_sub']			= 'Итого';
 $_['text_total']				= 'Всего';
