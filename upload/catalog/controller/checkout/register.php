17b13fd38f7de635ac6087909bcad13d5b683ab3
Anton Kalochelitis <fire.anton@gmail.com>
2020-03-01 23:19:20 +0200

    Отключаем ненужные поля Адреса для быстрого прохождения при оформлении заказа;

--- upload/catalog/controller/checkout/register.php
+++ upload/catalog/controller/checkout/register.php
@@ -134,29 +134,29 @@ class ControllerCheckoutRegister extends Controller {
 				$json['error']['telephone'] = $this->language->get('error_telephone');
 			}
 
-			if ((utf8_strlen(trim($this->request->post['address_1'])) < 3) || (utf8_strlen(trim($this->request->post['address_1'])) > 128)) {
-				$json['error']['address_1'] = $this->language->get('error_address_1');
-			}
+//			if ((utf8_strlen(trim($this->request->post['address_1'])) < 3) || (utf8_strlen(trim($this->request->post['address_1'])) > 128)) {
+//				$json['error']['address_1'] = $this->language->get('error_address_1');
+//			}
 
-			if ((utf8_strlen(trim($this->request->post['city'])) < 2) || (utf8_strlen(trim($this->request->post['city'])) > 128)) {
-				$json['error']['city'] = $this->language->get('error_city');
-			}
+//			if ((utf8_strlen(trim($this->request->post['city'])) < 2) || (utf8_strlen(trim($this->request->post['city'])) > 128)) {
+//				$json['error']['city'] = $this->language->get('error_city');
+//			}
 
 			$this->load->model('localisation/country');
 
-			$country_info = $this->model_localisation_country->getCountry($this->request->post['country_id']);
+//			$country_info = $this->model_localisation_country->getCountry($this->request->post['country_id']);
 
-			if ($country_info && $country_info['postcode_required'] && (utf8_strlen(trim($this->request->post['postcode'])) < 2 || utf8_strlen(trim($this->request->post['postcode'])) > 10)) {
-				$json['error']['postcode'] = $this->language->get('error_postcode');
-			}
+//			if ($country_info && $country_info['postcode_required'] && (utf8_strlen(trim($this->request->post['postcode'])) < 2 || utf8_strlen(trim($this->request->post['postcode'])) > 10)) {
+//				$json['error']['postcode'] = $this->language->get('error_postcode');
+//			}
 
-			if ($this->request->post['country_id'] == '') {
-				$json['error']['country'] = $this->language->get('error_country');
-			}
+//			if ($this->request->post['country_id'] == '') {
+//				$json['error']['country'] = $this->language->get('error_country');
+//			}
 
-			if (!isset($this->request->post['zone_id']) || $this->request->post['zone_id'] == '' || !is_numeric($this->request->post['zone_id'])) {
-				$json['error']['zone'] = $this->language->get('error_zone');
-			}
+//			if (!isset($this->request->post['zone_id']) || $this->request->post['zone_id'] == '' || !is_numeric($this->request->post['zone_id'])) {
+//				$json['error']['zone'] = $this->language->get('error_zone');
+//			}
 
 			if ((utf8_strlen(html_entity_decode($this->request->post['password'], ENT_QUOTES, 'UTF-8')) < 4) || (utf8_strlen(html_entity_decode($this->request->post['password'], ENT_QUOTES, 'UTF-8')) > 40)) {
 				$json['error']['password'] = $this->language->get('error_password');
@@ -212,10 +212,10 @@ class ControllerCheckoutRegister extends Controller {
 			// Default Payment Address
 			$this->load->model('account/address');
 				
-			$address_id = $this->model_account_address->addAddress($customer_id, $this->request->post);
+//			$address_id = $this->model_account_address->addAddress($customer_id, $this->request->post);
 			
 			// Set the address as default
-			$this->model_account_customer->editAddressId($customer_id, $address_id);
+//			$this->model_account_customer->editAddressId($customer_id, $address_id);
 			
 			// Clear any previous login attempts for unregistered accounts.
 			$this->model_account_customer->deleteLoginAttempts($this->request->post['email']);
